f3acee30ca2111bbc2fb47b09e5b55ca5d579e00
Alex Eagle <alex@aspect.dev>
2023-02-09 15:18:28 -0800

    release: publish release artifact
    GitHub's stability guarantee for the archive is iffy, and we want metrics on downloads.
    See https://github.com/bazel-contrib/SIG-rules-authors/issues/11#issuecomment-1411222578

---
+++ .github/workflows/release_prep.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/env bash
+
+set -o errexit -o nounset -o pipefail
+
+# Set by GH actions, see
+# https://docs.github.com/en/actions/learn-github-actions/environment-variables#default-environment-variables
+TAG=${GITHUB_REF_NAME}
+# The prefix is chosen to match what GitHub generates for source archives
+PREFIX="rules_esbuild-${TAG:1}"
+ARCHIVE="rules_esbuild-$TAG.tar.gz"
+git archive --format=tar --prefix=${PREFIX}/ ${TAG} | gzip > $ARCHIVE
+SHA=$(shasum -a 256 $ARCHIVE | awk '{print $1}')
+
+cat << EOF
+## Using [Bzlmod] with Bazel 6:
+
+Add to your \`MODULE.bazel\` file:
+
+\`\`\`starlark
+bazel_dep(name = "aspect_rules_esbuild", version = "${TAG:1}")
+\`\`\`
+
+[Bzlmod]: https://bazel.build/build/bzlmod
+
+## Using WORKSPACE
+
+\`\`\`starlark
+load("@bazel_tools//tools/build_defs/repo:http.bzl", "http_archive")
+http_archive(
+    name = "aspect_rules_esbuild",
+    sha256 = "${SHA}",
+    strip_prefix = "${PREFIX}",
+    url = "https://github.com/aspect-build/rules_esbuild/releases/download/${TAG}/${ARCHIVE}",
+)
+EOF
+awk 'f;/--SNIP--/{f=1}' e2e/smoke/WORKSPACE.bazel
+echo "\`\`\`"
